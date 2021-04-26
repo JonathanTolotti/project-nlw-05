015f0d606b971f05333887ca06cfa48b2381f2e2
Jonathan Tolotti <jonathanstolotti@gmail.com>
2021-04-26 07:46:03 -0300

    first commit

---
+++ public/js/chat.js
@@ -0,0 +1,112 @@
+let socket_admin_id = null;
+let emailUser = null;
+let socket = null;
+
+document.querySelector("#start_chat").addEventListener("click", (event) => {
+  socket = io();
+
+  const chat_help = document.getElementById("chat_help");
+  chat_help.style.display = "none";
+
+  const chat_in_support = document.getElementById("chat_in_support");
+  chat_in_support.style.display = "block";
+
+  const btn_chat = document.getElementById("btn_support");
+  btn_chat.style.display = "none";
+
+  const email = document.getElementById("email").value;
+  emailUser = email;
+
+  const text = document.getElementById("txt_help").value;
+
+  socket.on("connect", () => {
+    const params = {
+      email,
+      text,
+    };
+    socket.emit("client_first_access", params, (call, err) => {
+      if (err) {
+        console.err(err);
+      } else {
+        console.log(call);
+      }
+    });
+  });
+
+  socket.on("client_list_all_messages", (messages) => {
+    var template_client = document.getElementById("message-user-template")
+      .innerHTML;
+    var template_admin = document.getElementById("admin-template").innerHTML;
+
+    messages.forEach((message) => {
+      if (message.admin_id === null) {
+        const rendered = Mustache.render(template_client, {
+          message: message.text,
+          email,
+        });
+
+        document.getElementById("messages").innerHTML += rendered;
+      } else {
+        const rendered = Mustache.render(template_admin, {
+          message_admin: message.text,
+        });
+
+        document.getElementById("messages").innerHTML += rendered;
+      }
+    });
+
+    const divSupport = document.querySelector(".text_support");
+    divSupport.scrollTop = divSupport.scrollHeight;
+  });
+
+  socket.on("admin_send_to_client", (message) => {
+    const template_admin = document.getElementById("admin-template").innerHTML;
+
+    const rendered = Mustache.render(template_admin, {
+      message_admin: message.text,
+    });
+
+    document.getElementById("messages").innerHTML += rendered;
+
+    const divSupport = document.querySelector(".text_support");
+    divSupport.scrollTop = divSupport.scrollHeight;
+  });
+
+  socket.on("admin_join_chat", (adminID) => {
+    socket_admin_id = adminID;
+  });
+
+  socket.on("admin_left_chat", () => {
+    let element = document.querySelector(".message_send");
+    element.remove();
+  });
+
+});
+
+document
+  .querySelector("#send_message_button")
+  .addEventListener("click", (event) => {
+    const text = document.getElementById("message_user");
+
+    const params = {
+      text: text.value,
+      socket_admin_id,
+    };
+
+    socket.emit("client_send_to_admin", params);
+
+    const template_client = document.getElementById("message-user-template")
+      .innerHTML;
+
+    const rendered = Mustache.render(template_client, {
+      message: text.value,
+      email: emailUser,
+    });
+
+    document.getElementById("messages").innerHTML += rendered;
+
+    text.value = "";
+
+    const divSupport = document.querySelector(".text_support");
+    divSupport.scrollTop = divSupport.scrollHeight;
+  });
